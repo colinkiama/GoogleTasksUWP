c8eb69745058a92e2c7ad511ccd7bc6105db0f37
Colin Kiama <colinkiama@gmail.com>
2019-02-17 17:33:03 +0000

    Added resource containers

---
+++ GoogleTasksUWPAPI/GTaskListsContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GoogleTasksUWPAPI
+{
+    public sealed class GTaskListsContainer
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag")]
+        public string ETag { get; set; }
+
+        [JsonProperty("nextPageToken")]
+        public string NextPageToken { get; set; }
+
+        [JsonProperty("items")]
+        public GTaskList[] Items { get; set; }
+    }
+}
--- GoogleTasksUWPAPI/GTasksClient.cs
+++ GoogleTasksUWPAPI/GTasksClient.cs
@@ -1,18 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace GoogleTasksUWPAPI
 {
     public sealed class GTasksClient
     {
-        readonly Uri _tasksEndpointUri = new Uri("https://www.googleapis.com/tasks/v1");
+        private readonly Uri _tasksEndpointUri = new Uri("https://www.googleapis.com/tasks/v1");
+
+        private const string TokenScheme = "Bearer";
+        public Token Token { get; set; }              
+
 
         public GTasksClient(Token token)
         {
-
+            Token = token;
         }
+
+        #region Tasklists
+
+        public IAsyncOperation<GTaskListsContainer> ListTaskListsAsync()
+        {
+            return ListTaskListsTask().AsAsyncOperation();
+        }
+
+        internal async Task<GTaskListsContainer> ListTaskListsTask()
+        {
+            const string requestUri = "https://www.googleapis.com/tasks/v1/users/@me/lists";
+            GTaskListsContainer taskListsContainer = new GTaskListsContainer();
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(TokenScheme, Token.AccessToken);
+
+            var responseMessage = await client.GetAsync(requestUri);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                taskListsContainer = JsonConvert.DeserializeObject<GTaskListsContainer>(responseJson);
+            }
+            return taskListsContainer;
+        }
+        #endregion
+
+        #region Tasks
+
+        
+        #endregion
+
     }
 }
